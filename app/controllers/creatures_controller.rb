cec46fef3695477b2a242dfa4c8c1c55f24b7abb
dweb1 <david.p.weber@gmail.com>
2017-09-06 10:39:30 -0400

    Creature create and show views and routes working

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -4,4 +4,25 @@ class CreaturesController < ApplicationController
         @creatures = Creature.all
     end
 
+    def new
+        @creature = Creature.new
+    end
+
+    def create 
+        creature = Creature.new(creature_params)
+        if creature.save
+            redirect_to creature_path(creature)
+        else
+            redirect_to new_creature_path
+        end
+    end
+
+    def show
+        @creature = Creature.find(params[:id])
+    end
+
+    private
+    def creature_params
+       params.require(:creature).permit(:name, :description)
+    end
 end
